cef01d7be23ad302094e26ac9856b895a697586a
Tanguy Strub <tanguystrub08@gmail.com>
2023-07-28 01:39:59 +0200

    Ajout de SASS et de la police

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Header from './components/Header'
-import Home from './components/Home'
-import Footer from './components/Footer'
+
+//Pages
+import Home from './pages/Home'
+
+//Components
+
+
+//Style
+import './main.css'
+
 
 const Router = () => {
     return (
         <BrowserRouter>
-            <Header />
             <Routes>
                 <Route path="/" element={<Home />} />    
             </Routes>
-            <Footer />
         </BrowserRouter>
     );
 };
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Logo from '../../assets/Logo.svg';
+import { NavLink } from 'react-router-dom';
+
+
+function Header() {
+  return (
+    <header className='header'>
+      <div className='logo-container'>
+        <img src={Logo} alt=''></img>
+      </div>
+      <nav className='navigation'>
+        <ul className='navigation-lien-style'>
+          <li>
+            <NavLink to="/">Accueil</NavLink>
+          </li>
+          <li>
+            <NavLink to="/">A propos</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
